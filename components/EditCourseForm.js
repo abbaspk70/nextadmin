992bf69d3d0052d41b5c3d5a3e224adb49c946ef
abbaspk70 <abbaspk70@hotmail.com>
2023-08-07 00:01:14 +0500

    updated env

--- components/EditCourseForm.js
+++ components/EditCourseForm.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 export default function EditCourseForm() {
   return (
-    <div className='bg-accent/10'>
-        <form>
-            <input className='block' type='text' value={'title'} placeholder='Title'/>
-            <button className='bg-secondary px-5 py-3'  type='submit'>Update</button>
-        </form>
+    <div className='bg-accent/10 text-black'>
+        <div>from edit page</div>
     </div>
   )
 }
