689d5f8ca5479dedc83727f299330ae2001a080f
abbaspk70 <abbaspk70@hotmail.com>
2023-08-07 10:26:43 +0500

    register page turn into client

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,12 +1,14 @@
+'use client'
 import RegisterForm from '@/components/RegisterForm';
-import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+// import { authOptions } from '../api/auth/[...nextauth]/route';
+// import { getServerSession } from 'next-auth';
+import { useSession } from 'next-auth/react';
 
 
-export default async function page() {
-        const session = await getServerSession(authOptions);
-
+export default function page() {
+        // const session = await getServerSession(authOptions);
+        const {data: session} =  useSession();
         if(session) redirect("/dashboard")
 
     return (<div className='flex h-screen justify-center items-center mx-auto w-full'><RegisterForm /></div>
