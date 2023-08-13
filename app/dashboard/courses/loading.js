61abf947d2ec7219ecf1e4f310f822db9b5e16a8
abbaspk70 <abbaspk70@hotmail.com>
2023-08-13 23:18:41 +0500

    removed courses staff student

--- app/dashboard/courses/loading.js
+++
@@ -1,9 +0,0 @@
-import PageLoader from "@/components/loaders/PageLoader"
-export default function page() {
-    return (
-      <div className='md:ml-[300px] h-full'>
-      <PageLoader/>
-        </div>
-     
-    )
-  }
--- app/dashboard/create/course/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CreateCourseForm from '@/components/CreateCourseForm'
-export default function page() {
-  return (
-    <div className='flex h-screen justify-center items-center mx-auto w-full'>
-        <CreateCourseForm/>
-    </div>
-  )
-}
